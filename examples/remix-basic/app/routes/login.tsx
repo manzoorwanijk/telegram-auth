096237a518b3c0eb4b390fbbf5a914fcd173bc14
Manzoor Wani <manzoorwani.jk@gmail.com>
2022-12-25 20:24:35 +0530

    Add remix example (#2)
    * Remove examples from workspace packages
    
    * Expose server utils
    
    * Revert "Remove examples from workspace packages"
    
    This reverts commit 4aa39565c8fdac3ea00e7d91386e988e930503ca.
    
    * Add types to server
    
    * Add remix basic package

---
+++ examples/remix-basic/app/routes/login.tsx
@@ -0,0 +1,46 @@
+import { json, redirect } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import type { LoaderArgs, ActionFunction } from '@remix-run/node';
+import { LoginButton } from '@telgram-auth/react';
+
+import { getSession, commitSession, destroySession } from '../sessions';
+
+export async function loader({ request }: LoaderArgs) {
+	const session = await getSession(request.headers.get('Cookie'));
+
+	if (session.has('user')) {
+		return redirect('/');
+	}
+
+	const botUsername = process.env.BOT_USERNAME || '';
+
+	const data = { error: session.get('error'), botUsername };
+
+	return json(data, {
+		headers: {
+			'Set-Cookie': await commitSession(session),
+		},
+	});
+}
+
+export const action: ActionFunction = async ({ request }) => {
+	const session = await getSession(request.headers.get('Cookie'));
+
+	return redirect('/', {
+		headers: {
+			'Set-Cookie': await destroySession(session),
+		},
+	});
+};
+
+export default function Login() {
+	const { botUsername, error } = useLoaderData();
+
+	return (
+		<div>
+			{error ? <div className="error">{error}</div> : null}
+			<br />
+			<LoginButton botUsername={botUsername} authCallbackUrl="/auth" />
+		</div>
+	);
+}
