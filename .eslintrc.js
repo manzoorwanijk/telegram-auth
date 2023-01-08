770b5077657e801a8e322aaed7fcdf857ce06bee
Manzoor Wani <manzoorwani.jk@gmail.com>
2023-01-08 19:49:37 +0000

    Update package names

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,5 @@
 module.exports = {
 	root: true,
 	// This tells ESLint to load the config from the package `@telgram-auth/eslint-config`
-	extends: ['@telgram-auth/eslint-config/recommended'],
+	extends: ['@telegram-auth/eslint-config/recommended'],
 };
